6f0ee09ae4f66f1db117898af830eb0b146d279a
“Enrique <enrique.apple.id@gmail.com>
2020-05-30 02:07:24 +0000

    created user program that page faults and allocates and mappes a new page to the user stack

--- test2.c
+++ test2.c
@@ -3,13 +3,17 @@
 
 int main(int argc, char** argv)
 {
-    int size = 6192;
+    //According to lab 3, for now, your trap handler should simply check if the page fault was caused by an access to the page right under the current top of the stack
+
+    int size = 6192; //6192 > 4092(PGSIZE) && 6192 < 8192(2*PGSIZE), ergo the address is to the page right under the current top of the stack
 
     if (argc > 1)
-        size = 8192;
+        size = 8192; //8192 = 4092*2 = PGSIZE*2
 
     char buffer[size];
     memset(buffer, 0, size); 
+
+    printf(1, "If there is no kernal panic, then the page right under the current top of the stack wass allocated and mapped. SUCCESS!");
 
     exit();
     return 0;
